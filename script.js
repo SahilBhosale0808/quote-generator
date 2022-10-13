1af8ecd541bea7c2fa377fe6e98f0d1705493cfb
Sahil Bhosale <sahilbhosale0808@gmail.com>
2022-10-13 19:43:20 +0530

    Initial commit

--- script.js
+++ script.js
@@ -7,41 +7,37 @@ const loader = document.getElementById("loader");
 
 let apiQuotes = [];
 
-// Loading Spinner Shown
 function loading() {
   loader.hidden = false;
   quoteContainer.hidden = true;
 }
 
-// Remove Loading Spinner
+
 function complete() {
   quoteContainer.hidden = false;
   loader.hidden = true;
 }
 
-// Show New Quote
 function newQuote() {
   loading();
-  // Pick a random quote from array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  // Check if Author field is blank and replace it with 'Unknown'
   if (!quote.author) {
     authorText.textContent = "Unknown";
   } else {
     authorText.textContent = quote.author;
   }
-  // Check Quote length to determine styling
+
+
   if (quote.text.length > 120) {
     quoteText.classList.add("long-quote");
   } else {
     quoteText.classList.remove("long-quote");
   }
-  // Set Quote, Hide Loader
+
   quoteText.textContent = quote.text;
   complete();
 }
 
-// Get Quotes From API
 async function getQuotes() {
   loading();
   const apiUrl = "https://type.fit/api/quotes";
@@ -54,15 +50,14 @@ async function getQuotes() {
   }
 }
 
-// Tweet Quote
 function tweetQuote() {
   const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.innerText} - ${authorText.innerText}`;
   window.open(twitterUrl, "_blank");
 }
 
-// Event Listeners
+
 newQuoteBtn.addEventListener("click", newQuote);
 twitterBtn.addEventListener("click", tweetQuote);
 
-// On Load
+
 getQuotes();
